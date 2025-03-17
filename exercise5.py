01b80a1994734c4f899bc45ae434fb1e8655166c
yaoyao <202283890009@nuist.edu.cn>
2025-03-17 16:27:13 +0800

    added exercise5.py

---
+++ exercise5.py
@@ -0,0 +1,11 @@
+number = int(input("Enter a number to calculate its factorial: "))
+
+if number < 0:    
+	print("Factorial is not defined for negative numbers.")
+else:    
+	result = 1   
+	while number > 1:        
+		result *= number        
+		number -= 1    
+    
+print(f"The factorial of the given number is {result}.")
